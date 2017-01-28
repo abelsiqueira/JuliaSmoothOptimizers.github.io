89ae85dcc290db1d0ef6799101ec375966d338ae
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2017-01-28 10:09:32 -0200

    Change to Native HTML of Documenter.jl

---
+++ search_index.js
@@ -0,0 +1,163 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#[Julia-Smooth-Optimizers](https://JuliaSmoothOptimizers.github.io)-[![](assets/github.png)](https://github.com/JuliaSmoothOptimizers/)-1",
+    "page": "Home",
+    "title": "Julia Smooth Optimizers - (Image: )",
+    "category": "section",
+    "text": "Here you will find a collection of Julia packages for Nonlinear Optimization. These packages provide the tools needed for creating optimization problems and methods, but also provide access for optimization problems and methods.See each package status here."
+},
+
+{
+    "location": "index.html#Optimization-Problems-1",
+    "page": "Home",
+    "title": "Optimization Problems",
+    "category": "section",
+    "text": "NLPModels.jl: Defines data structures for nonlinear optimization models. Includes models for user-provided functions, automatic differentiation, JuMP / MathProgBase models, and the abstract models used by CUTEst.jl and AmplNLReader.jl.\nCUTEst.jl: Provides access to the Constrained and Unconstrained Testing Environment with safe threads (CUTEST) repository of problems. Uses NLPModels.jl.\nAmplNLReader.jl: Provides access to AMPL Models. Uses NLPModels.jl.\nOptimizationProblems.jl: Implements several problem in JuMP format. Can be used with NLPModels for easy multiplatform access to problems."
+},
+
+{
+    "location": "index.html#Tools-1",
+    "page": "Home",
+    "title": "Tools",
+    "category": "section",
+    "text": "Krylov.jl: Implements Hand-Picked Krylov methods. For instance, Steihaug-Toint conjugate-gradient method for the minimization of a non-convex quadratic in a Trust-Region.\nLinearOperators.jl: Implements linear operators, facilitating the use of some operations (like Hessian-vector products or Limited BFGS matrices). Specially useful with Krylov methods.\nBenchmarkProfiles.jl: Implements performance profile for Julia. Uses and easy input and access the Plots library, enabling the use of various backends."
+},
+
+{
+    "location": "index.html#Methods-1",
+    "page": "Home",
+    "title": "Methods",
+    "category": "section",
+    "text": "Optimize.jl: Optimization Algorithms in Pure Julia. The focus is to provide large-scale efficient and robust methods. Uses NLPModels for unified access to all problems above. Also defines tools for benchmarking and profiling codes using the same API."
+},
+
+{
+    "location": "index.html#Interfaces-1",
+    "page": "Home",
+    "title": "Interfaces",
+    "category": "section",
+    "text": "AMD.jl: Julia Interface to the AMD library of Amestoy, Davis and Duff.\nHSL.jl: Julia Interface to the HSL Mathematical Software Library.\nMUMPS.jl: Julia Interface to MUMPS.\nPROPACK.jl: Julia wrapper of the PROPACK sparse SVD library\nqr_mumps.jl: Interface to multicore QR factorization qr_mumps."
+},
+
+{
+    "location": "status.html#",
+    "page": "Status",
+    "title": "Status",
+    "category": "page",
+    "text": "This is a brief list of the installed packages and their situation. For help, check the docs of each package,   gitter for   anyone online, or open an issue on GitHub"
+},
+
+{
+    "location": "status.html#AMD.jl-1",
+    "page": "Status",
+    "title": "AMD.jl",
+    "category": "section",
+    "text": "Approximate Minimum Degree Ordering in Julia(Image: Github Stars) (Image: )  (Image: )  (Image: ) "
+},
+
+{
+    "location": "status.html#AmplNLReader.jl-1",
+    "page": "Status",
+    "title": "AmplNLReader.jl",
+    "category": "section",
+    "text": "Julia AMPL Models Conforming to NLPModels.jl(Image: Github Stars) (Image: )  (Image: )  (Image: ) "
+},
+
+{
+    "location": "status.html#BenchmarkProfiles.jl-1",
+    "page": "Status",
+    "title": "BenchmarkProfiles.jl",
+    "category": "section",
+    "text": "Performance and data profiles(Image: Github Stars) (Image: )  (Image: )  (Image: ) "
+},
+
+{
+    "location": "status.html#CUTEst.jl-1",
+    "page": "Status",
+    "title": "CUTEst.jl",
+    "category": "section",
+    "text": "Julia's CUTEst Interface(Image: Github Stars) (Image: )  (Image: )  (Image: )"
+},
+
+{
+    "location": "status.html#HSL.jl-1",
+    "page": "Status",
+    "title": "HSL.jl",
+    "category": "section",
+    "text": "Julia interface to the HSL Mathematical Software Library(Image: Github Stars) (Image: )  (Image: ) "
+},
+
+{
+    "location": "status.html#Krylov.jl-1",
+    "page": "Status",
+    "title": "Krylov.jl",
+    "category": "section",
+    "text": "A Julia Basket of Hand-Picked Krylov Methods(Image: Github Stars) (Image: ) (Image: )  (Image: ) (Image: )  (Image: ) (Image: ) "
+},
+
+{
+    "location": "status.html#LinearOperators.jl-1",
+    "page": "Status",
+    "title": "LinearOperators.jl",
+    "category": "section",
+    "text": "Linear Operators for Julia(Image: Github Stars) (Image: ) (Image: )  (Image: ) (Image: )  (Image: )"
+},
+
+{
+    "location": "status.html#MUMPS.jl-1",
+    "page": "Status",
+    "title": "MUMPS.jl",
+    "category": "section",
+    "text": "A Julia Interface to MUMPS(Image: Github Stars) (Image: )  (Image: ) "
+},
+
+{
+    "location": "status.html#NLPModels.jl-1",
+    "page": "Status",
+    "title": "NLPModels.jl",
+    "category": "section",
+    "text": "Data Structures for Optimization Models(Image: Github Stars) (Image: )  (Image: )  (Image: )  (Image: )"
+},
+
+{
+    "location": "status.html#OptimizationProblems.jl-1",
+    "page": "Status",
+    "title": "OptimizationProblems.jl",
+    "category": "section",
+    "text": "Optimization Problems for Julia(Image: Github Stars) (Image: )  (Image: )  (Image: ) "
+},
+
+{
+    "location": "status.html#Optimize.jl-1",
+    "page": "Status",
+    "title": "Optimize.jl",
+    "category": "section",
+    "text": "Optimization Algorithms in Pure Julia(Image: Github Stars) (Image: )  (Image: )  (Image: ) "
+},
+
+{
+    "location": "status.html#PROPACK.jl-1",
+    "page": "Status",
+    "title": "PROPACK.jl",
+    "category": "section",
+    "text": "Julia wrapper of the PROPACK sparse SVD library(Image: Github Stars) (Image: )  (Image: )  (Image: ) "
+},
+
+{
+    "location": "status.html#qr_mumps.jl-1",
+    "page": "Status",
+    "title": "qr_mumps.jl",
+    "category": "section",
+    "text": "Interface to multicore QR factorization qr_mumps(Image: Github Stars) (Image: )  (Image: ) "
+},
+
+]}
